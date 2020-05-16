5475009f660c48fafc99cb740710da4dc3f72cab
keisuke.kato <kgulliverk@hotmail.com>
2020-05-17 00:01:24 +0900

    バリデーション追加(password)

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-ActiveRecord::Schema.define(version: 2020_05_16_142331) do
+ActiveRecord::Schema.define(version: 2020_05_16_144606) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2020_05_16_142331) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "parts", "stories"
